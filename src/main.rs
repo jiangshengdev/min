231f5af1f0161ff797c77ff81d8b4ddd4ec7bd0d
Jiang Sheng <jiangshengdev@outlook.com>
2022-06-18 19:29:23 +0800

    First

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 #![no_main]
 #![feature(alloc_error_handler)]
 
+extern crate alloc;
+
 use core::arch::global_asm;
 
+mod first;
 mod heap;
 mod panic;
 mod test;
@@ -12,5 +15,6 @@ global_asm!(include_str!("entry.s"));
 
 #[no_mangle]
 fn main() {
+    first::test::basics();
     test::exit();
 }
